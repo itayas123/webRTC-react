6c5f5b7deb0e9267fa9d38a6d83678cafc113b9e
Itay Asher <itayasher@gmail.com>
2019-11-13 11:36:15 +0100

    add some videos

--- src/UI/components/MediaScreen/VideoPanel/videoPanel.js
+++ src/UI/components/MediaScreen/VideoPanel/videoPanel.js
@@ -1,29 +1,38 @@
 import "./videoPanel.css";
 import React from "react";
+import videoSrc from "./videos/Welcome.mp4";
+import videoSrc2 from "./videos/2.mp4";
 
 export default class VideoPanel extends React.Component {
   renderVideos() {
+    console.log(this.state);
     switch (this.props.split) {
       case 1:
         return (
           <div className="wh100p border-2p pointer">
-            {this.props.videoArray && this.props.videoArray[0]
-              ? this.props.videoArray[0]
-              : ""}
+            {this.props.videoArray && this.props.videoArray[0] ? (
+              <video autoPlay className="video" controls src={videoSrc} />
+            ) : (
+              ""
+            )}
           </div>
         );
       case 2:
         return (
           <div className="wh100p border flex pointer">
             <div className="width-50p border">
-              {this.props.videoArray && this.props.videoArray[0]
-                ? this.props.videoArray[0]
-                : ""}
+              {this.props.videoArray && this.props.videoArray[0] ? (
+                <video autoPlay className="video" controls src={videoSrc} />
+              ) : (
+                ""
+              )}
             </div>
             <div className="width-50p border">
-              {this.props.videoArray && this.props.videoArray[1]
-                ? this.props.videoArray[1]
-                : ""}
+              {this.props.videoArray && this.props.videoArray[1] ? (
+                <video autoPlay className="video" controls src={videoSrc2} />
+              ) : (
+                ""
+              )}
             </div>
           </div>
         );
@@ -31,14 +40,18 @@ export default class VideoPanel extends React.Component {
         return (
           <div className="wh100p border pointer">
             <div className="height-50p border">
-              {this.props.videoArray && this.props.videoArray[0]
-                ? this.props.videoArray[0]
-                : ""}
+              {this.props.videoArray && this.props.videoArray[0] ? (
+                <video autoPlay className="video" controls src={videoSrc} />
+              ) : (
+                ""
+              )}
             </div>
             <div className="height-50p border">
-              {this.props.videoArray && this.props.videoArray[1]
-                ? this.props.videoArray[1]
-                : ""}
+              {this.props.videoArray && this.props.videoArray[1] ? (
+                <video autoPlay className="video" controls src={videoSrc2} />
+              ) : (
+                ""
+              )}
             </div>
           </div>
         );
@@ -47,26 +60,34 @@ export default class VideoPanel extends React.Component {
           <div className="wh100p border pointer">
             <div className="height-50p flex">
               <div className="width-50p border">
-                {this.props.videoArray && this.props.videoArray[0]
-                  ? this.props.videoArray[0]
-                  : ""}
+                {this.props.videoArray && this.props.videoArray[0] ? (
+                  <video autoPlay className="video" controls src={videoSrc} />
+                ) : (
+                  ""
+                )}
               </div>
               <div className="width-50p border">
-                {this.props.videoArray && this.props.videoArray[1]
-                  ? this.props.videoArray[1]
-                  : ""}
+                {this.props.videoArray && this.props.videoArray[1] ? (
+                  <video autoPlay className="video" controls src={videoSrc2} />
+                ) : (
+                  ""
+                )}
               </div>
             </div>
             <div className="height-50p flex">
               <div className="width-50p border">
-                {this.props.videoArray && this.props.videoArray[2]
-                  ? this.props.videoArray[2]
-                  : ""}
+                {this.props.videoArray && this.props.videoArray[2] ? (
+                  <video autoPlay className="video" controls src={videoSrc} />
+                ) : (
+                  ""
+                )}
               </div>
               <div className="width-50p border">
-                {this.props.videoArray && this.props.videoArray[3]
-                  ? this.props.videoArray[3]
-                  : ""}
+                {this.props.videoArray && this.props.videoArray[3] ? (
+                  <video autoPlay className="video" controls src={videoSrc2} />
+                ) : (
+                  ""
+                )}
               </div>
             </div>
           </div>
